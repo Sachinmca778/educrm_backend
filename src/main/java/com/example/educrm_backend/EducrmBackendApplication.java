773f53247205a84e5730e48821bebdf13988e8c5
Sachin <sahanisachin60@gmail.com>
2025-06-28 19:56:21 +0530

    project setup

---
+++ src/main/java/com/example/educrm_backend/EducrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.educrm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducrmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/educrm_backend/EducrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educrm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
